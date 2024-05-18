a5e120c66503c91cf6cf680976e0cb1a861484e1
Triano <trianorsebastian@gmail.com>
2024-05-18 20:52:10 -0300

    Django_CRM

--- website/views.py
+++ website/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
+
+
 
 def home(request):
+	records=Record.objects.all()
     # el POST tiene que estar escrito en mayúsculas, sino no funciona!
 	if request.method=='POST':
 		username=request.POST['username']
@@ -16,7 +21,7 @@ def home(request):
 			messages.success(request,'There was an error logging in. Please try again.')
 			return redirect('home')
 	else:
-		return render(request,'home.html',{})
+		return render(request,'home.html',{'records':records})
 
 # def login_user(request):
 # 	return render(request,'home.html',{})
@@ -26,6 +31,69 @@ def logout_user(request):
 	messages.success(request,'You have been logged out.')
 	return redirect('home')
 
+
 def register_user(request):
-    return render(request,'register.html',{})
-    
+	if request.method=='POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username=form.cleaned_data['username']
+			password=form.cleaned_data['password1']
+			user=authenticate(username=username,password=password)
+			login(request,user)
+			messages.success(request,'You have been successfully registered.')
+			return redirect('home')
+	else:
+		form=SignUpForm()
+		return render(request,'register.html',{'form':form})
+
+	return render(request,'register.html',{'form':form})
+
+
+def customer_record(request,pk):
+	if request.user.is_authenticated:
+		customer_record=Record.objects.get(id=pk)
+		return render(request,'record.html',{'customer_record':customer_record})
+	else:
+		messages.success(request,'You must be logged in to view that page.')
+		return redirect('home')
+
+
+def delete_record(request,pk):
+	if request.user.is_authenticated:
+		delete_it=Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request,'Record Deleted Successfully')
+		return redirect('home')
+	else:
+		messages.success(request,'You must be logged in to delete records.')
+		return redirect('home')
+
+
+def add_record(request):
+	form=AddRecordForm(request.POST or None)
+	if request.user.is_authenticated:
+		if request.method=='POST':
+			if form.is_valid():
+				add_record=form.save()
+				return redirect('home')
+		return render(request,'add_record.html',{'form':form})
+	else:
+		messages.success(request,'You must be logged in.')
+		return redirect('home')
+
+
+def update_record(request,pk):
+	if request.user.is_authenticated:
+		current_record=Record.objects.get(id=pk)
+		form=AddRecordForm(request.POST or None,instance=current_record)
+		if form.is_valid():
+			form.save()
+			messages.success(request,'Record had been updated.')
+			return redirect('home')
+		return render(request,'update_record.html',{'form':form})
+	else:
+		messages.success(request,'You must be logged in.')
+		return redirect('home')
+
+        
